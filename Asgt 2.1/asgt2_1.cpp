f2a813e84a26f2e3f8640e8a737a2ab1442698cd
Oki <gtracer465@gmail.com>
2023-06-18 12:46:44 -0400

    Add original output + fix alg.

--- Asgt 2.1/asgt2_1.cpp
+++ Asgt 2.1/asgt2_1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void selectionSort(int array[], int length) {
+void selectionSort(int array[], int& length) {
     int maxIndex = 0;
     int swapTemp = 0;
     for (int i = 0; i <= length-2; i++) {
         maxIndex = i;
-        for (int j = i+1; j <= length-1; j++) {
+        for (int j = i+1; j <= length-i; j++) {
             if (array[j] > array[maxIndex]) {
                 maxIndex = j;
             }
@@ -17,7 +17,7 @@ void selectionSort(int array[], int length) {
     }
 }
 
-void readInput(int length, int array[]) {
+void readInput(int& length, int array[]) {
     cout << "Enter the length of the array:" << endl;
     cin >> length;
     for (int i = 0; i < length; i++) {
@@ -26,18 +26,21 @@ void readInput(int length, int array[]) {
     }
 }
 
-void output(int array[], int length) {
-    cout << "The sorted list is:" << endl;
+void output(int array[], int& length) {
     for (int i = 0; i < length; i++) {
         cout << array[i] << " ";
     }
+    cout << endl;
 }
 
 int main() {
-    int array[] = {0};
+    int array[99] = {0};
     int length = 0;
     readInput(length, array);
+    cout << "The original array is: " << endl;
+    output(array, length);
     selectionSort(array, length);
+    cout << "The sorted array is: " << endl;
     output(array, length);
 
     return 0;
