b1c41f7c5ad77341bbc7e711ad90bb1e5174fc3d
okikohaku <gtracer465@gmail.cim>
2023-06-24 11:30:21 -0400

    Class restructure, finish 2.4

--- Asgt 2/Asgt 2.4/asgt2_4.cpp
+++ Asgt 2/Asgt 2.4/asgt2_4.cpp
@@ -1,52 +1,57 @@
 #include <iostream>
 using namespace std;
 
-void readVariables(int& a, int& k, int& n) {
-    cout << "Enter a value for a:" << endl;
-    cin >> a;
-    cout << "Enter a value for k:" << endl;
-    cin >> k;
-    cout << "Enter a value for n:" << endl;
-    cin >> n;
-}
+class Variables {
 
-void binaryK(int& k, int K[]) {
-    int temp = k;
-    for (int i = 0; temp > 0; i++) {
-        K[i] = temp % 2;
-        temp = (temp-K[i]/2);
-    }
-}
-
-int modularExpo(int& a, int K[], int& n) {
-    if (n == 1) {
-        return 0;
-    }
-    int b = 1;
-    if (K == 0) {
-        return b;
-    }
-    int A = a;
-    if (K[0] == 1) {
-        b = a;
-    }
-    for (int i = 0; i < 9; i++) {
-        A = A * A % n;
-        if (K[i] == 1) {
-            b = A*b % n;
-        }
-    }
-    return b;
-}
-
-int main() {
     int a = 0;
     int k = 0;
     int n = 0;
     int K[99] = {0};
-    cout << "This program calculates a^k mod n. " << endl;
-    readVariables(a, k, n);
-    binaryK(k, K);
-    cout << "Result: " << modularExpo(a, K, n) << endl;
-    return 0;
+
+public:
+    void readVariables() {
+        cout << "Enter a value for a:" << endl;
+        cin >> a;
+        cout << "Enter a value for k:" << endl;
+        cin >> k;
+        cout << "Enter a value for n:" << endl;
+        cin >> n;
+    }
+
+    void binaryK() {
+        int temp = k;
+        int i = 0;
+        while (temp > 0) {
+            K[i] = temp % 2;
+            temp = (temp - K[i]) / 2;
+            i++;
+        }
+    }
+
+    int modularExpo() {
+        if (n == 1) {
+            return 0;
+        }
+        int b = 1;
+        int A = a;
+        if (K[0] == 1) {
+            b = a;
+        }
+        for (int i = 1; i < sizeof(K) / sizeof(int); i++) {
+            A = A * A % n;
+            if (K[i] == 1) {
+                b = A * b % n;
+            }
+        }
+        return b;
+    }
 };
+
+    int main() {
+        Variables vars;
+        cout << "This program calculates a^k mod n. " << endl;
+        vars.readVariables();
+        vars.binaryK();
+        cout << "Result: " << vars.modularExpo() << endl;
+        return 0;
+    };
