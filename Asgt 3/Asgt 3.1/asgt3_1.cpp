fefc39e7185c8046e44a281d643efe0cc9c1bce7
okikohaku <gtracer465@gmail.cim>
2023-07-17 13:23:04 -0400

    Create list, print, and insert

--- Asgt 3/Asgt 3.1/asgt3_1.cpp
+++ Asgt 3/Asgt 3.1/asgt3_1.cpp
@@ -10,11 +10,20 @@ public:
 Node* start = nullptr;
 
 Node* getNode() {
-    Node* newnode;
-    newnode= (Node*)malloc(sizeof(Node));
-    cout << "Enter data (type int):" << endl;
-    cin >> &newnode->value;
-    return newnode;
+    Node* newNode;
+    newNode= (Node*)malloc(sizeof(Node));
+    cout << "Enter data (type char):" << endl;
+    cin >> &newNode->value;
+    return newNode;
+}
+
+void printList(Node* start) {
+    Node* currentNode = start;
+    while (currentNode != nullptr) {
+        cout << currentNode->value;
+        currentNode = currentNode->next;
+    }
+    cout << endl;
 }
 
 void createList(int n) {
@@ -34,19 +43,51 @@ void createList(int n) {
     }
 }
 
-void insertBeg() {
+int countNode(Node* start) {
+    Node* temp = start;
+    static int nodeCount = 1;
+    while (temp->next != nullptr) {
+        nodeCount++;
+        temp = temp->next;
+    }
+    return nodeCount;
+}
+
+void insertMid() {
     Node* newNode;
+    Node* temp;
+    Node* prev;
+    int pos = 0;
+    int nodectr = 0;
+    int ctr = 1;
     newNode = getNode();
-    if (start == nullptr) {
-        start = newNode;
+    cout << "Enter position: " << endl;
+    scanf("%d", &pos);
+    nodectr = countNode(start);
+    if (pos > 1 && pos < nodectr) {
+        temp = prev = start;
+        while (ctr < pos) {
+            prev = temp;
+            temp = temp->next;
+            ctr++;
+        }
+        prev->next = newNode;
+        newNode->next = temp;
     } else {
-        newNode->next = start;
-        start = newNode;
+        cout << "Position %d is not a middle position" << endl;
     }
 }
 
 int main() {
-    Node* start = nullptr;
+    int n = 0;
+    cout << "Enter length of linked list: " << endl;
+    cin >> n;
+    createList(n);
+    printList(start);
+    cout << "Now inserting data into list. " << endl;
+    insertMid();
+    printList(start);
+
 
     return 0;
 }
