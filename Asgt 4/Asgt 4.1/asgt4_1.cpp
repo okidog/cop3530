c1bf3381e449fdc5541ec57fba3580a6c301aed5
Oki <gtracer465@gmail.com>
2023-07-31 15:06:54 -0400

    Final commit for 4.1

--- Asgt 4/Asgt 4.1/asgt4_1.cpp
+++ Asgt 4/Asgt 4.1/asgt4_1.cpp
@@ -1,3 +1,67 @@
-//
-// Created by adrie on 7/31/2023.
-//
+#include <iostream>
+using namespace std;
+
+// Init stack
+const int MAX = 10; // Max will not change in code, but can be changed for different implementations
+int stack[MAX];
+int top = 0;
+
+int menu() {
+    int choice;
+    cout << "1. Push\n2. Pop\n3. Display\n4. Quit" << endl;
+    cin >> choice;
+    return choice;
+}
+
+void display() {
+    if (top == 0) {
+        cout << "The stack is empty!" << endl;
+    } else {
+        for (int i = 0; i < top; i++) {
+            cout << "pos " << i+1 << ": "<< stack[i] << endl;
+        }
+    }
+}
+
+void pop() {
+    if (top == 0) {
+        cout << "Stack empty, operation skipped" << endl;
+    } else {
+        cout << stack[--top] << endl;
+    }
+}
+
+void push() {
+    int data = 0;
+    if (top == MAX) {
+        cout << "Stack is full!" << endl;
+        return;
+    } else {
+        cout << "Enter data:" << endl;
+        cin >> data;
+        stack[top] = data;
+        ++top;
+        cout << "Data pushed successfully." << endl;
+        return;
+    }
+}
+
+int main() {
+    int choice = 0;
+    do {
+        choice = menu();
+        switch(choice) {
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                return 0;
+        }
+    } while (true);
+}
