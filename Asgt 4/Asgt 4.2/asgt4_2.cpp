649999902784cf217150baad03c5563c55591c81
Oki <gtracer465@gmail.com>
2023-08-01 18:10:23 -0400

    Hardcoded list

--- Asgt 4/Asgt 4.2/asgt4_2.cpp
+++ Asgt 4/Asgt 4.2/asgt4_2.cpp
@@ -19,32 +19,21 @@ const int MAX = 10; // Max will not change in code, but can be changed for diffe
 int stack[MAX];
 int top = 0;
 
-void display() {
-    if (top == 0) {
-        cout << "The stack is empty!" << endl;
-    } else {
-        for (int i = 0; i < top; i++) {
-            cout << "pos " << i+1 << ": "<< stack[i] << endl;
-        }
-    }
-}
-
-void pop() {
+// Same code from 4.1 with minor tweaks
+char pop() {
     if (top == 0) {
         cout << "Stack empty, operation skipped" << endl;
     } else {
-        cout << stack[--top];
+        cout << stack[--top] << ' ';
+        return stack[top]; // Return character for use in display funct
     }
 }
 
-void push() {
-    int data = 0;
+void push(char data) { // Now accepts input, removed user input
     if (top == MAX) {
         cout << "Stack is full!" << endl;
         return;
     } else {
-        cout << "Enter data:" << endl;
-        cin >> data;
         stack[top] = data;
         ++top;
         cout << "Data pushed successfully." << endl;
@@ -52,15 +41,17 @@ void push() {
     }
 }
 
-Node* getNode() {
-    Node* newNode;
+Node* getNode() { // 3.2 function, but instead of using user input, uses chars from alphabet[] in creation order
+    Node* newNode; // This results in a tree with the same order as example I.2 from lec 14
     newNode= (Node*)malloc(sizeof(Node));
     newNode->value = alphabet[alphaPos];
-    newNode->leftChild = nullptr; // Same funct as 3.1, two lines added to init leftChild/rightChild values to nullptr
+    alphaPos++;
+    newNode->leftChild = nullptr;
     newNode->rightChild = nullptr;
     return newNode;
 }
 
+/* Unused for now
 void createList(int depth) {
     Node* newNode;
     Node* temp;
@@ -68,7 +59,6 @@ void createList(int depth) {
         newNode = getNode();
         if(start == nullptr) {
             start = newNode;
-            alphaPos++;
         } else {
             temp = start;
             while (temp->rightChild != nullptr) {
@@ -79,18 +69,31 @@ void createList(int depth) {
         }
     }
 }
+*/
 
 void printList(Node* start) { // Same funct as 3.1
     Node* currentNode = start;
-    while (currentNode != nullptr) { // Loop until rightChild pointer is nullptr, signifying end
-        cout << currentNode->value;
-        currentNode = currentNode->rightChild;
+    while (currentNode != nullptr) { // Loop until leftChild is nullptr, signifying end of tree depth-wise
+        push(currentNode->value);
+        currentNode = currentNode->leftChild;
     }
+    pop();
+    pop();
+    currentNode = start->leftChild->rightChild;
+    push(currentNode->value);
+    pop();
     cout << endl;
 }
 
 
 int main() {
+    Node* root = getNode();
+    root->leftChild = getNode();
+    root->rightChild = getNode();
+    root->leftChild->leftChild = getNode();
+    root->leftChild->rightChild = getNode();
+    root->rightChild->leftChild = getNode();
+    root->rightChild->rightChild = getNode();
 
     return 0;
 }
